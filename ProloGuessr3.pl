30ddb293f453d7882e818675e5899e10e3741805
Jorge Alberto <javalle1101@gmail.com>
2025-09-20 12:03:57 -0300

    Add files via upload

---
+++ ProloGuessr3.pl
@@ -0,0 +1,239 @@
+
+pais(brasil, [hemisferio_sul, america_do_sul, portugues, graus(25), populacao(215)]).
+pais(franca, [hemisferio_norte, europa, frances, graus(12), populacao(65)]).
+pais(reino_unido, [hemisferio_norte, europa, ingles, graus(9), populacao(67)]).
+pais(italia, [hemisferio_norte, europa, italiano, graus(15), populacao(59)]).
+pais(alemanha, [hemisferio_norte, europa, alemao, graus(9), populacao(83)]).
+pais(espanha, [hemisferio_norte, europa, espanhol, graus(15), populacao(47)]).
+pais(russia, [hemisferio_norte, europa, russo, graus(-5), populacao(144)]).
+pais(grecia, [hemisferio_norte, europa, grego, graus(19), populacao(10.4)]).
+pais(paises_baixos, [hemisferio_norte, europa, holandes, graus(10), populacao(17.7)]).
+pais(china, [hemisferio_norte, asia, mandarim, graus(10), populacao(1412)]).
+pais(japao, [hemisferio_norte, asia, japones, graus(15), populacao(125)]).
+pais(india, [hemisferio_norte, asia, hindi, graus(24), populacao(1408)]).
+pais(coreia_do_sul, [hemisferio_norte, asia, coreano, graus(13), populacao(51)]).
+pais(tailandia, [hemisferio_norte, asia, tailandes, graus(28), populacao(71)]).
+pais(egito, [hemisferio_norte, africa, arabe, graus(22), populacao(109)]).
+pais(quenia, [hemisferio_sul, africa, ingles, graus(25), populacao(54)]).
+pais(estados_unidos, [hemisferio_norte, america_do_norte, ingles, graus(11), populacao(332)]).
+pais(canada, [hemisferio_norte, america_do_norte, ingles, graus(-5), populacao(38)]).
+pais(mexico, [hemisferio_norte, america_do_norte, espanhol, graus(21), populacao(126)]).
+pais(argentina, [hemisferio_sul, america_do_sul, espanhol, graus(15), populacao(45)]).
+pais(peru, [hemisferio_sul, america_do_sul, espanhol, graus(20), populacao(33)]).
+pais(chile, [hemisferio_sul, america_do_sul, espanhol, graus(10), populacao(19)]).
+pais(venezuela, [hemisferio_norte, america_do_sul, espanhol, graus(27), populacao(28)]).
+pais(colombia, [hemisferio_norte, america_do_sul, espanhol, graus(24), populacao(51)]).
+pais(africa_do_sul, [hemisferio_sul, africa, ingles, graus(17), populacao(59)]).
+pais(australia, [hemisferio_sul, oceania, ingles, graus(22), populacao(26)]).
+pais(nova_zelandia, [hemisferio_sul, oceania, ingles, graus(13), populacao(5.1)]).
+pais(turquia, [hemisferio_norte, asia, turco, graus(14), populacao(85)]).
+pais(ira, [hemisferio_norte, asia, persa, graus(18), populacao(88)]).
+pais(israel, [hemisferio_norte, asia, hebraico, graus(20), populacao(9.5)]).
+pais(indonesia, [hemisferio_sul, asia, indonesio, graus(26), populacao(275)]).
+pais(paquistao, [hemisferio_norte, asia, urdu, graus(21), populacao(236)]).
+pais(nigeria, [hemisferio_norte, africa, ingles, graus(27), populacao(219)]).
+pais(bangladesh, [hemisferio_norte, asia, bengali, graus(26), populacao(171)]).
+pais(etiopia, [hemisferio_norte, africa, amarico, graus(22), populacao(123)]).
+pais(filipinas, [hemisferio_norte, asia, filipino, graus(27), populacao(115)]).
+pais(vietna, [hemisferio_norte, asia, vietnamita, graus(24), populacao(98)]).
+pais(rd_congo, [hemisferio_sul, africa, frances, graus(25), populacao(99)]).
+pais(tanzania, [hemisferio_sul, africa, suaíli, graus(23), populacao(65)]).
+pais(mianmar, [hemisferio_norte, asia, birmanes, graus(23), populacao(54)]).
+pais(uganda, [hemisferio_norte, africa, ingles, graus(23), populacao(47)]).
+pais(argelia, [hemisferio_norte, africa, arabe, graus(23), populacao(45)]).
+pais(sudao, [hemisferio_norte, africa, arabe, graus(29), populacao(47)]).
+pais(ucrania, [hemisferio_norte, europa, ucraniano, graus(8), populacao(43)]).
+pais(iraque, [hemisferio_norte, asia, arabe, graus(22), populacao(44)]).
+pais(afeganistao, [hemisferio_norte, asia, pashto, graus(13), populacao(41)]).
+pais(polonia, [hemisferio_norte, europa, polones, graus(8), populacao(38)]).
+pais(marrocos, [hemisferio_norte, africa, arabe, graus(17), populacao(37)]).
+pais(arabia_saudita, [hemisferio_norte, asia, arabe, graus(26), populacao(36)]).
+pais(uzbequistao, [hemisferio_norte, asia, uzbeque, graus(12), populacao(35)]).
+pais(angola, [hemisferio_sul, africa, portugues, graus(22), populacao(35)]).
+pais(malasia, [hemisferio_norte, asia, malaio, graus(27), populacao(33)]).
+pais(mocambique, [hemisferio_sul, africa, portugues, graus(24), populacao(32)]).
+pais(gana, [hemisferio_norte, africa, ingles, graus(28), populacao(33)]).
+pais(iemen, [hemisferio_norte, asia, arabe, graus(25), populacao(33)]).
+pais(nepal, [hemisferio_norte, asia, nepales, graus(15), populacao(30)]).
+pais(madagascar, [hemisferio_sul, africa, malgaxe, graus(22), populacao(29)]).
+pais(camaroes, [hemisferio_norte, africa, frances, graus(25), populacao(28)]).
+pais(costa_do_marfim, [hemisferio_norte, africa, frances, graus(27), populacao(28)]).
+pais(coreia_do_norte, [hemisferio_norte, asia, coreano, graus(6), populacao(26)]).
+pais(niger, [hemisferio_norte, africa, frances, graus(29), populacao(26)]).
+pais(taiwan, [hemisferio_norte, asia, mandarim, graus(23), populacao(24)]).
+pais(siria, [hemisferio_norte, asia, arabe, graus(18), populacao(22)]).
+pais(mali, [hemisferio_norte, africa, frances, graus(29), populacao(22)]).
+pais(burkina_faso, [hemisferio_norte, africa, frances, graus(29), populacao(22)]).
+pais(sri_lanka, [hemisferio_norte, asia, cingales, graus(27), populacao(22)]).
+pais(malawi, [hemisferio_sul, africa, ingles, graus(22), populacao(20)]).
+pais(zambia, [hemisferio_sul, africa, ingles, graus(21), populacao(20)]).
+pais(cazaquistao, [hemisferio_norte, asia, cazaque, graus(6), populacao(19)]).
+pais(romenia, [hemisferio_norte, europa, romeno, graus(10), populacao(19)]).
+pais(equador, [hemisferio_sul, america_do_sul, espanhol, graus(21), populacao(18)]).
+pais(guatemala, [hemisferio_norte, america_do_norte, espanhol, graus(22), populacao(18)]).
+pais(senegal, [hemisferio_norte, africa, frances, graus(28), populacao(17)]).
+pais(camboja, [hemisferio_norte, asia, khmer, graus(28), populacao(17)]).
+pais(zimbabue, [hemisferio_sul, africa, ingles, graus(21), populacao(16)]).
+
+capital(brasilia, brasil).
+capital(paris, franca).
+capital(londres, reino_unido).
+capital(roma, italia).
+capital(berlim, alemanha).
+capital(madri, espanha).
+capital(moscou, russia).
+capital(atenas, grecia).
+capital(amsterda, paises_baixos).
+capital(pequim, china).
+capital(toquio, japao).
+capital(nova_delhi, india).
+capital(seul, coreia_do_sul).
+capital(bangkok, tailandia).
+capital(cairo, egito).
+capital(nairobi, quenia).
+capital(washington_dc, estados_unidos).
+capital(ottawa, canada).
+capital(cidade_do_mexico, mexico).
+capital(buenos_aires, argentina).
+capital(lima, peru).
+capital(santiago, chile).
+capital(caracas, venezuela).
+capital(bogota, colombia).
+capital(pretoria, africa_do_sul).
+capital(camberra, australia).
+capital(wellington, nova_zelandia).
+capital(ancara, turquia).
+capital(teera, ira).
+capital(jerusalem, israel).
+capital(jacarta, indonesia).
+capital(islamaba, paquistao).
+capital(abuja, nigeria).
+capital(daca, bangladesh).
+capital(adis_abeba, etiopia).
+capital(manila, filipinas).
+capital(hanoi, vietna).
+capital(kinshasa, rd_congo).
+capital(dodoma, tanzania).
+capital(nay_pyi_taw, mianmar).
+capital(kampala, uganda).
+capital(argel, argelia).
+capital(cartum, sudao).
+capital(kiev, ucrania).
+capital(bagda, iraque).
+capital(cabul, afeganistao).
+capital(varsovia, polonia).
+capital(rabat, marrocos).
+capital(riade, arabia_saudita).
+capital(tashkent, ubequistao).
+capital(luanda, angola).
+capital(kuala_lumpur, malasia).
+capital(maputo, mocambique).
+capital(acra, gana).
+capital(sana, iemen).
+capital(katmandu, nepal).
+capital(antananarivo, madagascar).
+capital(yaunde, camaroes).
+capital(yamoussoukro, costa_do_marfim).
+capital(pyongyang, coreia_do_norte).
+capital(niamey, niger).
+capital(taipei, taiwan).
+capital(damasco, siria).
+capital(bamako, mali).
+capital(ouagadougou, burkina_faso).
+capital(colombo, sri_lanka).
+capital(lilongwe, malawi).
+capital(lusaka, zambia).
+capital(astana, cazaquistao).
+capital(bucareste, romenia).
+capital(quito, equador).
+capital(cidade_da_guatemala, guatemala).
+capital(dakar, senegal).
+capital(phnom_penh, camboja).
+capital(harare, zimbabue).
+
+listar(Atributos):-
+    findall(Pais, (
+                pais(Pais, AtrPais),
+                forall(member(Atr, Atributos), member(Atr, AtrPais))
+                ),
+            Paises),
+
+    ( Paises = []
+    -> write('Nenhum país correspondente.'), nl
+    ;  write('Países possíveis:'), nl,
+       forall(member(Pais, Paises), (
+           write(Pais), nl
+       ))
+    ).
+
+carac(Pais):-
+    pais(Pais, Carac),
+    write(Carac), nl.
+
+:- dynamic pais_correto/1.
+
+iniciar_jogo :-
+    findall(P, pais(P, _), Paises),
+    random_member(Correto, Paises),
+    retractall(pais_correto(_)),
+    asserta(pais_correto(Correto)),
+    write('Jogo iniciado! Tente adivinhar o país.'), nl.
+
+e_numerico(graus(_)).
+e_numerico(populacao(_)).
+
+verificar_palpite(Palpite) :-
+    pais_correto(Correto),
+    pais(Palpite, AtributosPalpite),
+    pais(Correto, AtributosCorreta),
+
+    exclude(e_numerico, AtributosPalpite, CategoricosPalpite),
+    exclude(e_numerico, AtributosCorreta, CategoricosCorreta),
+    intersection(CategoricosPalpite, CategoricosCorreta, AtributosComuns),
+    format('Atributos em comum: ~w~n', [AtributosComuns]),
+
+    member(graus(TempPalpite), AtributosPalpite),
+    member(graus(TempCorreta), AtributosCorreta),
+    (   TempPalpite =:= TempCorreta
+    ->  write('-> Temperatura: Você acertou a temperatura!')
+    ;   TempPalpite < TempCorreta
+    ->  write('-> Temperatura: A do país correto é MAIOR.')
+    ;   write('-> Temperatura: A do país correto é MENOR.')
+    ),
+    nl,
+
+    member(populacao(PopPalpite), AtributosPalpite),
+    member(populacao(PopCorreta), AtributosCorreta),
+    (   PopPalpite =:= PopCorreta
+    ->  write('-> População: Você acertou a população!')
+    ;   PopPalpite < PopCorreta
+    ->  write('-> População: A do país correto é MAIOR.')
+    ;   write('-> População: A do país correto é MENOR.')
+    ),
+    nl.
+
+jogo :-
+    iniciar_jogo,
+    loop_palpites.
+
+loop_palpites :-
+    write('> Seu palpite: '),
+    read(Palpite),
+    pais_correto(Correto),
+    (   Palpite == Correto
+    ->  write('Parabéns! Você acertou! O país era '), write(Correto), nl
+    ;   (    Palpite == 'desisto')
+        ->  write('Você desistiu! O país era '), write(Correto), nl
+    ;   (    Palpite == 'capital')
+        -> capital(CapCorreto, Correto),
+           write('A capital do país é '), write(CapCorreto), nl,
+           loop_palpites
+    ;   (   \+ pais(Palpite, _)
+        ->  write('Esse país não está no meu banco de dados. Tente outro.'), nl,
+            loop_palpites
+        ;   verificar_palpite(Palpite),
+            loop_palpites
+        )
+    ).
+
+%implementar dicas de letras
