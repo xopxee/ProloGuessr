8a3f7fc8581f4c78845dfbf87753744ad91a63b2
Jorge Alberto <javalle1101@gmail.com>
2025-09-20 22:13:16 -0300

    Adição de 100+ paises.pl

--- paises.pl
+++ paises.pl
@@ -27,7 +27,6 @@ pais(australia, [hemisferio_sul, oceania, ingles, graus(22), populacao(26)]).
 pais(nova_zelandia, [hemisferio_sul, oceania, ingles, graus(13), populacao(5.1)]).
 pais(turquia, [hemisferio_norte, asia, turco, graus(14), populacao(85)]).
 pais(ira, [hemisferio_norte, asia, persa, graus(18), populacao(88)]).
-pais(israel, [hemisferio_norte, asia, hebraico, graus(20), populacao(9.5)]).
 pais(indonesia, [hemisferio_sul, asia, indonesio, graus(26), populacao(275)]).
 pais(paquistao, [hemisferio_norte, asia, urdu, graus(21), populacao(236)]).
 pais(nigeria, [hemisferio_norte, africa, ingles, graus(27), populacao(219)]).
@@ -36,7 +35,7 @@ pais(etiopia, [hemisferio_norte, africa, amarico, graus(22), populacao(123)]).
 pais(filipinas, [hemisferio_norte, asia, filipino, graus(27), populacao(115)]).
 pais(vietna, [hemisferio_norte, asia, vietnamita, graus(24), populacao(98)]).
 pais(rd_congo, [hemisferio_sul, africa, frances, graus(25), populacao(99)]).
-pais(tanzania, [hemisferio_sul, africa, sua�li, graus(23), populacao(65)]).
+pais(tanzania, [hemisferio_sul, africa, suaíli, graus(23), populacao(65)]).
 pais(mianmar, [hemisferio_norte, asia, birmanes, graus(23), populacao(54)]).
 pais(uganda, [hemisferio_norte, africa, ingles, graus(23), populacao(47)]).
 pais(argelia, [hemisferio_norte, africa, arabe, graus(23), populacao(45)]).
@@ -69,7 +68,95 @@ pais(zambia, [hemisferio_sul, africa, ingles, graus(21), populacao(20)]).
 pais(cazaquistao, [hemisferio_norte, asia, cazaque, graus(6), populacao(19)]).
 pais(romenia, [hemisferio_norte, europa, romeno, graus(10), populacao(19)]).
 pais(equador, [hemisferio_sul, america_do_sul, espanhol, graus(21), populacao(18)]).
-pais(guatemala, [hemisferio_norte, america_do_norte, espanhol, graus(22), populacao(18)]).
+pais(guatemala, [hemisferio_norte, america_central, espanhol, graus(22), populacao(18)]).
 pais(senegal, [hemisferio_norte, africa, frances, graus(28), populacao(17)]).
 pais(camboja, [hemisferio_norte, asia, khmer, graus(28), populacao(17)]).
 pais(zimbabue, [hemisferio_sul, africa, ingles, graus(21), populacao(16)]).
+pais(noruega, [hemisferio_norte, europa, noruegues, graus(3), populacao(5.5)]).
+pais(suecia, [hemisferio_norte, europa, sueco, graus(4), populacao(10.5)]).
+pais(finlandia, [hemisferio_norte, europa, finlandes, graus(2), populacao(5.6)]).
+pais(dinamarca, [hemisferio_norte, europa, dinamarques, graus(8), populacao(5.9)]).
+pais(portugal, [hemisferio_norte, europa, portugues, graus(15), populacao(10.3)]).
+pais(belgica, [hemisferio_norte, europa, frances, holandes, alemao, graus(10), populacao(11.7)]).
+pais(islandia, [hemisferio_norte, europa, islandes, graus(2), populacao(0.4)]).
+pais(irlanda, [hemisferio_norte, europa, ingles, irlandes, graus(9), populacao(5.1)]).
+pais(bulgaria, [hemisferio_norte, europa, bulgaro, graus(11), populacao(6.8)]).
+pais(hungria, [hemisferio_norte, europa, hungaro, graus(10), populacao(9.6)]).
+pais(croacia, [hemisferio_norte, europa, croata, graus(13), populacao(3.9)]).
+pais(servia, [hemisferio_norte, europa, servo, graus(12), populacao(6.7)]).
+pais(suica, [hemisferio_norte, europa, alemao, frances, italiano, romanche, graus(9), populacao(8.7)]).
+pais(bosnia, [hemisferio_norte, europa, bosnio, servo, croata, graus(11), populacao(3.3)]).
+pais(austria, [hemisferio_norte, europa, alemao, graus(8), populacao(9.1)]).
+pais(eslovaquia, [hemisferio_norte, europa, eslovaco, graus(9), populacao(5.4)]).
+pais(eslovenia, [hemisferio_norte, europa, esloveno, graus(10), populacao(2.1)]).
+pais(republica_tcheca, [hemisferio_norte, europa, tcheco, graus(8), populacao(10.7)]).
+
+pais(estonia, [hemisferio_norte, europa, estoniano, graus(6), populacao(1.3)]).
+pais(lituania, [hemisferio_norte, europa, lituano, graus(7), populacao(2.7)]).
+pais(letonia, [hemisferio_norte, europa, letao, graus(6), populacao(1.9)]).
+
+pais(quirguistao, [hemisferio_norte, asia, quirguiz, russo, graus(5), populacao(6.7)]).
+pais(cuba, [hemisferio_norte, america_central, espanhol, graus(25), populacao(11.3)]).
+pais(honduras, [hemisferio_norte, america_central, espanhol, graus(25), populacao(10.1)]).
+pais(el_salvador, [hemisferio_norte, america_central, espanhol, graus(24), populacao(6.3)]).
+pais(laos, [hemisferio_norte, asia, laosiano, graus(25), populacao(7.5)]).
+pais(jamaica, [hemisferio_norte, america_central, ingles, graus(26), populacao(2.8)]).
+pais(haiti, [hemisferio_norte, america_central, frances, crioulo_haitiano, graus(25), populacao(11.4)]).
+pais(republica_dominicana, [hemisferio_norte, america_central, espanhol, graus(25), populacao(11.1)]).
+pais(bolivia, [hemisferio_sul, america_do_sul, espanhol, quichua, aimara, graus(18), populacao(12.1)]).
+pais(uruguai, [hemisferio_sul, america_do_sul, espanhol, graus(17), populacao(3.5)]).
+pais(paraguai, [hemisferio_sul, america_do_sul, espanhol, guarani, graus(23), populacao(7.4)]).
+pais(suriname, [hemisferio_sul, america_do_sul, holandes, graus(26), populacao(0.6)]).
+pais(guiana, [hemisferio_sul, america_do_sul, ingles, graus(26), populacao(0.8)]).
+pais(panama, [hemisferio_norte, america_central, espanhol, graus(27), populacao(4.4)]).
+pais(costa_rica, [hemisferio_norte, america_central, espanhol, graus(25), populacao(5.2)]).
+pais(albania, [hemisferio_norte, europa, albanes, graus(15), populacao(2.8)]).
+pais(kosovo, [hemisferio_norte, europa, albanes, servo, graus(12), populacao(1.8)]).
+pais(macedonia_do_norte, [hemisferio_norte, europa, macedonio, graus(12), populacao(2.1)]).
+pais(luxemburgo,[hemisferio_norte,europa,frances,alemao,luxemburgues,graus(9),populacao(0.6)]).
+pais(liechtenstein,[hemisferio_norte,europa,alemao,graus(8),populacao(0.04)]).
+pais(monaco,[hemisferio_norte,europa,frances,graus(16),populacao(0.04)]).
+pais(andorra,[hemisferio_norte,europa,catalao,graus(9),populacao(0.08)]).
+pais(malta,[hemisferio_norte,europa,maltes,ingles,graus(19),populacao(0.5)]).
+pais(san_marino,[hemisferio_norte,europa,italiano,graus(14),populacao(0.03)]).
+pais(vaticano,[hemisferio_norte,europa,italiano,latim,graus(16),populacao(0.0008)]).
+pais(bielorrussia,[hemisferio_norte,europa,bielorrusso,russo,graus(6),populacao(9.4)]).
+pais(moldavia,[hemisferio_norte,europa,romeno,graus(10),populacao(4.0)]).
+pais(montenegro,[hemisferio_norte,europa,montenegrino,graus(13),populacao(0.6)]).
+pais(georgia,[hemisferio_norte,europa,asia,georgiano,graus(11),populacao(3.7)]).
+pais(armenia,[hemisferio_norte,europa,asia,armênio,graus(10),populacao(3.0)]).
+pais(azerbaijao,[hemisferio_norte,europa,asia,azeri,graus(12),populacao(10.1)]).
+pais(benin,[hemisferio_norte,africa,frances,graus(28),populacao(13.0)]).
+pais(togo,[hemisferio_norte,africa,frances,graus(27),populacao(8.8)]).
+pais(ruanda,[hemisferio_sul,africa,ruandes,frances,ingles,graus(20),populacao(13.8)]).
+pais(burundi,[hemisferio_sul,africa,rundi,frances,graus(21),populacao(12.6)]).
+pais(chade,[hemisferio_norte,africa,frances,arabe,graus(28),populacao(17.2)]).
+pais(republica_centro_africana,[hemisferio_norte,africa,frances,sango,graus(26),populacao(5.5)]).
+pais(liberia,[hemisferio_norte,africa,ingles,graus(26),populacao(5.2)]).
+pais(serra_leoa,[hemisferio_norte,africa,ingles,graus(26),populacao(8.1)]).
+pais(guine,[hemisferio_norte,africa,frances,graus(26),populacao(13.5)]).
+pais(guine_bissau,[hemisferio_norte,africa,portugues,graus(27),populacao(2.0)]).
+pais(guine_equatorial,[hemisferio_norte,africa,espanhol,frances,portugues,graus(25),populacao(1.5)]).
+pais(gabao,[hemisferio_sul,africa,frances,graus(25),populacao(2.3)]).
+pais(congo,[hemisferio_sul,africa,frances,graus(25),populacao(5.8)]).
+pais(lesoto,[hemisferio_sul,africa,soto,ingles,graus(11),populacao(2.2)]).
+pais(essuatini,[hemisferio_sul,africa,swazi,ingles,graus(19),populacao(1.2)]).
+pais(mauritania,[hemisferio_norte,africa,arabe,graus(28),populacao(4.9)]).
+pais(djibouti,[hemisferio_norte,africa,arabe,frances,graus(28),populacao(1.0)]).
+pais(eritreia,[hemisferio_norte,africa,tigrinya,arabe,graus(25),populacao(3.7)]).
+pais(somalia,[hemisferio_norte,africa,somali,arabe,graus(28),populacao(17.1)]).
+pais(mongolia,[hemisferio_norte,asia,mongol,graus(0),populacao(3.4)]).
+pais(singapura,[hemisferio_norte,asia,malaio,ingles,mandarim,tamil,graus(27),populacao(5.9)]).
+pais(brunei,[hemisferio_norte,asia,malaio,graus(27),populacao(0.5)]).
+pais(timor_leste,[hemisferio_sul,asia,portugues,tetum,graus(25),populacao(1.3)]).
+pais(butao,[hemisferio_norte,asia,drukpa,graus(10),populacao(0.8)]).
+pais(maldivas,[hemisferio_norte,asia,divehi,graus(28),populacao(0.5)]).
+pais(barein,[hemisferio_norte,asia,arabe,graus(28),populacao(1.5)]).
+pais(catar,[hemisferio_norte,asia,arabe,graus(28),populacao(2.7)]).
+pais(emirados_arabes,[hemisferio_norte,asia,arabe,graus(28),populacao(9.9)]).
+pais(oma,[hemisferio_norte,asia,arabe,graus(28),populacao(4.6)]).
+pais(jordania,[hemisferio_norte,asia,arabe,graus(20),populacao(11.2)]).
+pais(libano,[hemisferio_norte,asia,arabe,graus(20),populacao(6.8)]).
+pais(tajiquistao,[hemisferio_norte,asia,tajique,graus(8),populacao(9.8)]).
+pais(turcomenistao,[hemisferio_norte,asia,turcomeno,graus(15),populacao(6.3)]).
+pais(palestina,[hemisferio_norte,asia,arabe,graus(20),populacao(5.4)]).
