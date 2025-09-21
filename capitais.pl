22a18fe678bee71885407c821a96560b245e7959
Jorge Alberto <javalle1101@gmail.com>
2025-09-20 22:15:49 -0300

    Adição de 100+ capitais

--- capitais.pl
+++ capitais.pl
@@ -27,7 +27,6 @@ capital(camberra, australia).
 capital(wellington, nova_zelandia).
 capital(ancara, turquia).
 capital(teera, ira).
-capital(jerusalem, israel).
 capital(jacarta, indonesia).
 capital(islamaba, paquistao).
 capital(abuja, nigeria).
@@ -73,3 +72,89 @@ capital(cidade_da_guatemala, guatemala).
 capital(dakar, senegal).
 capital(phnom_penh, camboja).
 capital(harare, zimbabue).
+capital(oslo, noruega).
+capital(estocolmo, suecia).
+capital(helsinki, finlandia).
+capital(copenhague, dinamarca).
+capital(lisboa, portugal).
+capital(bruxelas, belgica).
+capital(reiquiavique, islandia).
+capital(dublin, irlanda).
+capital(sofia, bulgaria).
+capital(budapeste, hungria).
+capital(zagreb, croacia).
+capital(belgrado, servia).
+capital(berna, suica).
+capital(sarajevo, bosnia).
+capital(viena, austria).
+capital(bratislava, eslovaquia).
+capital(ljubljana, eslovenia).
+capital(praga, republica_tcheca).
+capital(tallinn, estonia).
+capital(vilnius, lituania).
+capital(riga, letonia).
+capital(bishkek, quirguistao).
+capital(havana, cuba).
+capital(tegucigalpa, honduras).
+capital(san_salvador, el_salvador).
+capital(vientiane, laos).
+capital(kingston, jamaica).
+capital(portauprince, haiti).
+capital(santo_domingo, republica_dominicana).
+capital(la_paz, bolivia).
+capital(montevideo, uruguai).
+capital(asuncion, paraguai).
+capital(paramaribo, suriname).
+capital(georgetown, guiana).
+capital(cidade_do_panama, panama).
+capital(san_jose, costa_rica).
+capital(tirana, albania).
+capital(pristina, kosovo).
+capital(skopje, macedonia_do_norte).
+capital(luxemburgo,luxemburgo).
+capital(vaduz,liechtenstein).
+capital(monaco,monaco).
+capital(andorra_a_velha,andorra).
+capital(valletta,malta).
+capital(san_marino,san_marino).
+capital(cidade_do_vaticano,vaticano).
+capital(minsk,bielorrussia).
+capital(chisinau,moldavia).
+capital(podgorica,montenegro).
+capital(tbilisi,georgia).
+capital(erevan,armenia).
+capital(baku,azerbaijao).
+capital(porto_novo,benin).
+capital(lome,togo).
+capital(kigali,ruanda).
+capital(bujumbura,burundi).
+capital(ndjamena,chade).
+capital(bangui,republica_centro_africana).
+capital(monrovia,liberia).
+capital(freetown,serra_leoa).
+capital(conakry,guine).
+capital(bissau,guine_bissau).
+capital(malabo,guine_equatorial).
+capital(libreville,gabao).
+capital(brazzaville,congo).
+capital(maseru,lesoto).
+capital(mbabane,essuatini).
+capital(nouakchott,mauritania).
+capital(djibouti,djibouti).
+capital(asmara,eritreia).
+capital(mogadishu,somalia).
+capital(ulaanbaatar,mongolia).
+capital(singapura,singapura).
+capital(bandar_seri_begawan,brunei).
+capital(dili,timor_leste).
+capital(thimphu,butao).
+capital(male,maldivas).
+capital(manama,barein).
+capital(doha,catar).
+capital(abu_dhabi,emirados_arabes).
+capital(mascate,oma).
+capital(amman,jordania).
+capital(beirute,libano).
+capital(dushanbe,tajiquistao).
+capital(ashgabat,turcomenistao).
+capital(jerusalem,palestina).
